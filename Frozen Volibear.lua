bb85dc01c81bf950f048f8c4e710c30ea2a44e69
Lillgoalie <alra@stockholmscience.se>
2014-05-31 15:10:02 +0200

    New version
    Added escape key and added settings for R and cleaned code.

--- Frozen Volibear.lua
+++ Frozen Volibear.lua
@@ -1,11 +1,6 @@
 --[[
  
         Frozen Volibear by Lillgoalie (+Ikita's W KS script)
-        Version: 1.0
-       
-        Features:
-			Combo Mode
-			KS with W
        
         Instructions on saving the file:
         - Save the file in scripts folder
@@ -15,6 +10,7 @@
 if myHero.charName ~= "Volibear" then return end
 
 local ts
+local Menu
 
 QRange, ERange, WRange, RRange = 600, 405, 400, 125
 player = GetMyHero()
@@ -24,25 +20,28 @@ function OnLoad()
 	ts = TargetSelector(TARGET_LOW_HP_PRIORITY,700)
 
 	-- Create the menu
-	Config = scriptConfig("Frozen Volibear by Lillgoalie", "FrozenVolibear")
+	Menu = scriptConfig("Frozen Volibear by Lillgoalie", "FrozenVolibear")
 	
-	Config:addTS(ts)
+	Menu:addTS(ts)
 	
-	Config:addSubMenu("["..myHero.charName.." - Combo]", "Combo")
-	Config.Combo:addParam("combo", "Combo mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-	Config.Combo:addParam("comboQ", "Use Q in Combo", SCRIPT_PARAM_ONOFF, true)
-	Config.Combo:addParam("comboW", "Use W in Combo", SCRIPT_PARAM_ONOFF, true)
-	Config.Combo:addParam("comboE", "Use E in Combo", SCRIPT_PARAM_ONOFF, true)
-	Config.Combo:addParam("comboR", "Use R in Combo", SCRIPT_PARAM_ONOFF, true)
+	Menu:addSubMenu("["..myHero.charName.." - VoliCombo]", "VoliCombo")
+	Menu.VoliCombo:addParam("combo", "VoliCombo mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+	Menu.VoliCombo:addParam("comboQ", "Use Q in VoliCombo", SCRIPT_PARAM_ONOFF, true)
+	Menu.VoliCombo:addParam("comboW", "Use W in VoliCombo", SCRIPT_PARAM_ONOFF, false)
+	Menu.VoliCombo:addParam("comboE", "Use E in VoliCombo", SCRIPT_PARAM_ONOFF, true)
+	Menu.VoliCombo:addSubMenu("Ultimate Settings", "Rset")
+	Menu.VoliCombo.Rset:addParam("comboR", "Use R in VoliCombo", SCRIPT_PARAM_ONOFF, true)
+	Menu.VoliCombo.Rset:addParam("MinimumR", "Minimum enemies to ult", SCRIPT_PARAM_SLICE, 2, 1, 5, 0)
 	
-	Config:addSubMenu("["..myHero.charName.." - KS]", "KS")
-	Config.KS:addParam("ks", "Enable Killsteal", SCRIPT_PARAM_ONOFF, true)
+	Menu:addSubMenu("["..myHero.charName.." - Additionals]", "Ads")
+	Menu.Ads:addParam("ks", "Enable Killsteal", SCRIPT_PARAM_ONOFF, true)
+	Menu.Ads:addParam("escape", "Escape key", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
 
-	Config:addSubMenu("["..myHero.charName.." - Drawings]", "drawings")
-	Config.drawings:addParam("drawCircleAA", "Draw AA Range", SCRIPT_PARAM_ONOFF, true)
-	Config.drawings:addParam("drawCircleQ", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
-	Config.drawings:addParam("drawCircleW", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
-	Config.drawings:addParam("drawCircleE", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
+	Menu:addSubMenu("["..myHero.charName.." - Drawings]", "drawings")
+	Menu.drawings:addParam("drawCircleAA", "Draw AA Range", SCRIPT_PARAM_ONOFF, true)
+	Menu.drawings:addParam("drawCircleQ", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
+	Menu.drawings:addParam("drawCircleW", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
+	Menu.drawings:addParam("drawCircleE", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
 	
 	-- Message
 	PrintChat("Loaded Frozen Volibear By Lillgoalie")
@@ -52,9 +51,17 @@ function OnTick()
 	if myHero.dead then return end
 	-- Check for enemies repeatly
 	ts:update()
-	
-	-- KS script from Ikita
-	if player:GetSpellData(_W).level > 0 and Config.KS.ks and player:CanUseSpell(_W) == READY then
+	KillSteal()
+
+	-- VoliCombo key pressed?
+	if (Menu.VoliCombo.combo) then
+		-- Activate function VoliCombo
+		VoliCombo()
+	end
+end
+
+function KillSteal()
+	if player:GetSpellData(_W).level > 0 and Menu.Ads.ks and player:CanUseSpell(_W) == READY then
 
 		for i=1, heroManager.iCount do
 			local target = heroManager:GetHero(i)
@@ -68,52 +75,53 @@ function OnTick()
 			end
 		end
 	end
-	
-	-- Combo key pressed?
-	if (Config.Combo.combo) then
-		-- Activate function Combo
-		Combo()
+end
+
+function Escape()
+	if myHero:CanUseSpell(_Q) then
+		CastSpell(_Q)
 	end
+	myHero:MoveTo(mousePos.x, mousePos.z)
 end
 			
-function Combo()
+function VoliCombo()
 	-- Can use spell, E in range and enabled?
-	if ValidTarget(ts.target, ERange) and myHero:CanUseSpell(_E) == READY and Config.Combo.comboE then
+	if ValidTarget(ts.target, ERange) and myHero:CanUseSpell(_E) == READY and Menu.VoliCombo.comboE then
 		-- Cast E
 		CastSpell(_E)
 	end
 	
 	-- Can use spell and range and enabled?
-	if ValidTarget(ts.target, QRange) and myHero:CanUseSpell(_Q) == READY and Config.Combo.comboQ then
+	if ValidTarget(ts.target, QRange) and myHero:CanUseSpell(_Q) == READY and Menu.VoliCombo.comboQ then
 		-- Cast Q
 		CastSpell(_Q)
 	end
 	-- Can use spell and range and enabled?
-	if ValidTarget(ts.target, RRange) and myHero:CanUseSpell(_R) == READY and Config.Combo.comboR then
+	if ValidTarget(ts.target, RRange) and CountEnemyHeroInRange(400) >= Menu.VoliCombo.Rset.MinimumR and myHero:CanUseSpell(_R) == READY and Menu.VoliCombo.comboR then
 		-- Cast R
 		CastSpell(_R)
 	end
 	-- Can use spell and range and enabled?
-	if ValidTarget(ts.target, WRange) and myHero:CanUseSpell(_W) == READY and Config.Combo.comboW then
+	if ValidTarget(ts.target, WRange) and myHero:CanUseSpell(_W) == READY and Menu.VoliCombo.comboW then
 		-- Cast W
 		CastSpell(_W, ts.target)
 	end
 end
 
 function OnDraw()
-	if Config.drawings.drawCircleAA then
+	if Menu.drawings.drawCircleAA then
 		DrawCircle(myHero.x, myHero.y, myHero.z, RRange, ARGB(255, 0, 255, 0))
 	end
 	
-	if Config.drawings.drawCircleQ then
+	if Menu.drawings.drawCircleQ then
 		DrawCircle(myHero.x, myHero.y, myHero.z, QRange, 0x111111)
 	end
 			
-	if Config.drawings.drawCircleW then
+	if Menu.drawings.drawCircleW then
 		DrawCircle(myHero.x, myHero.y, myHero.z, WRange, 0x111111)
 	end		
 
-	if Config.drawings.drawCircleE then
+	if Menu.drawings.drawCircleE then
 		DrawCircle(myHero.x, myHero.y, myHero.z, ERange, 0x111111)
 	end		
 end
